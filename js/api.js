8800445c325feda9ee6b9480e8b2a51805017dd8
leonardoffirmino <firmino750@gmail.com>
2023-11-29 22:32:44 -0300

    Finish get api

--- js/api.js
+++ js/api.js
@@ -3,10 +3,16 @@ const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_img');
 const form = document.querySelector('.form');
 const input = document.querySelector('.getPokemon');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+
+
+let searchPokemon = 1;
 
 const getFetchPokemon = async (pokemon) => {
 
-  const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+  const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
 
   if (apiResponse.status === 200) {
@@ -26,15 +32,18 @@ const renderPokemon = async (pokemon) => {
   const data = await getFetchPokemon(pokemon);
 
   if (data) {
+    pokemonImage.style.display = 'block';
     pokemonNumber.innerHTML = data.id;
     pokemonName.innerHTML = data.name;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
 
     input.value = "";
+    searchPokemon = data.id;
   } else {
     pokemonNumber.innerHTML = '';
     pokemonName.innerHTML = 'Not found item :/';
+    pokemonImage.style.display = 'none';
   }
 
 
@@ -46,10 +55,24 @@ const renderPokemon = async (pokemon) => {
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  renderPokemon(input.value);
+  renderPokemon(input.value.toLowerCase());
 
 });
 
-renderPokemon('1');
+
+buttonPrev.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+
+});
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+})
+
+renderPokemon(searchPokemon);
 
 
